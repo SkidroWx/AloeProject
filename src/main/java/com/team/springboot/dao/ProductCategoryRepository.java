70162e56b8008c0112310d77c5d754ae407383d7
SkiD <grgmhi61@gmail.com>
2021-07-06 15:35:22 +0300

    Initial commit: Basic structure and simple shape classes

---
+++ src/main/java/com/team/springboot/dao/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.team.springboot.dao;
+
+import com.team.springboot.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<Product, Long> {
+
+}
